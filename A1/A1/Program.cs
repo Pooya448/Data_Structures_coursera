dfc175f1b7d6019ed2b5d3848eabc23d972b4781
Pooya448 <pooya_kabiri@yahoo.com>
2018-10-04 13:36:00 +0330

    fix1

---
+++ A1/A1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static string Process(string input)
+        {
+            StringBuilder sb = new StringBuilder();
+            using (StringReader reader = new StringReader(input))
+            using (StringWriter writer = new StringWriter(sb))
+            {
+                int a = int.Parse(reader.ReadLine());
+                int b = int.Parse(reader.ReadLine());
+                writer.WriteLine(Add(a, b));
+            }
+            return sb.ToString();
+        }
+
+        public static int Add(int firstNum, int secondNum)
+        {
+            return firstNum + secondNum;
+        }
+    }
+}
---
+++ A1/A1Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("A1Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("A1Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d5b0f1b4-70fc-4abb-ba4b-2dc2be4f6724")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
