69287a840143e9aa4fbc74f88fa084db9acc0713
Pooya448 <pooya_kabiri@yahoo.com>
2018-11-01 18:57:00 +0330

    Q3 Done

--- A6/A6/EditDistance.cs
+++ A6/A6/EditDistance.cs
@@ -16,9 +16,37 @@ namespace A6
 
         public long Solve(string str1, string str2)
         {
-            
-            return 0;
+            int[,] DistanceEdit = new int[str1.Length + 1, str2.Length + 1];
+            DistanceEdit[0, 0] = 0;
+            for (int i = 1; i <= str1.Length; i++)
+                DistanceEdit[i, 0] = i;
+
+            for (int j = 1; j <= str2.Length; j++)
+                DistanceEdit[0, j] = j;
+
+            var iCount = DistanceEdit.GetLength(0);
+            var jCount = DistanceEdit.GetLength(1);
+
+            for (int i = 1; i < DistanceEdit.GetLength(0); i++)
+            {
+                for (int j = 1; j < DistanceEdit.GetLength(1); j++)
+                {
+                    int insertion = DistanceEdit[i, j - 1] + 1;
+                    int deletion = DistanceEdit[i - 1, j] + 1;
+                    int substitution = DistanceEdit[i - 1, j - 1] + 1;
+                    int match = DistanceEdit[i - 1, j - 1];
+                    if (str1[i - 1] == str2[j - 1])
+                        DistanceEdit[i, j] = Min(insertion, deletion, match);
+                    else
+                        DistanceEdit[i, j] = Min(insertion, deletion, substitution);
+                }
+            }
+            return DistanceEdit[str1.Length,str2.Length];
         }
 
+        private int Min(int insertion, int deletion, int matchOrmismatch)
+        {
+            return Math.Min(insertion, deletion) > matchOrmismatch ? matchOrmismatch : Math.Min(insertion, deletion);
+        }
     }
 }
--- A6/A6Exec/Program.cs
+++ A6/A6Exec/Program.cs
@@ -10,21 +10,49 @@ namespace A6Exec
     {
         static void Main(string[] args)
         {
-            int n = 5;
-            long[,] OptimumOperation = new long[2, n + 1];
-            OptimumOperation[0, 0] = 0;
-            for (int i = 2; i <= n; i++)
-            {
-                var TempResult = FindMin(OptimumOperation, i);
-                OptimumOperation[0, i] = TempResult.Item1 + 1;
-                OptimumOperation[1, i] = TempResult.Item2;
-            }
+            string str1 = "EDITING";
+            string str2 = "DISTANCE";
+            int[,] DistanceEdit = new int[str1.Length+1, str2.Length+1];
+            DistanceEdit[0, 0] = 0;
+            for (int i = 1; i <= str1.Length; i++)
+                DistanceEdit[i, 0] = i;
 
-            List<long> BacktrackList = new List<long>();
-            BacktrackList.Add(n);
-            Backtrack(OptimumOperation, n, ref BacktrackList);
-            BacktrackList.Reverse();
-            BacktrackList.ForEach(x => Console.WriteLine(x));
+            for (int j = 1; j <= str2.Length; j++)
+                DistanceEdit[0, j] = j;
+
+            var iCount = DistanceEdit.GetLength(0);
+            var jCount = DistanceEdit.GetLength(1);
+
+            for (int i = 1; i < DistanceEdit.GetLength(0); i++)
+            {
+                for (int j = 1; j < DistanceEdit.GetLength(1); j++)
+                {
+                    int insertion = DistanceEdit[i, j - 1] + 1;
+                    int deletion = DistanceEdit[i - 1, j] + 1;
+                    int substitution = DistanceEdit[i - 1, j - 1] + 1;
+                    int match = DistanceEdit[i - 1, j - 1];
+                    if (str1[i-1] == str2[j-1])
+                        DistanceEdit[i, j] = Min(insertion, deletion, match);
+                    else
+                        DistanceEdit[i, j] = Min(insertion, deletion, substitution);
+                }
+            }
+            Print2DArray(DistanceEdit);
+        }
+        public static int Min(int insertion, int deletion, int matchOrmismatch)
+        {
+            return Math.Min(insertion, deletion) > matchOrmismatch ? matchOrmismatch : Math.Min(insertion, deletion);
+        }
+        public static void Print2DArray (int[,] DistanceEdit)
+        {
+            var rowCount = DistanceEdit.GetLength(0);
+            var colCount = DistanceEdit.GetLength(1);
+            for (int row = 0; row < rowCount; row++)
+            {
+                for (int col = 0; col < colCount; col++)
+                    Console.Write(String.Format("{0}\t", DistanceEdit[row, col]));
+                Console.WriteLine();
+            }
             Console.Read();
         }
         public static void Backtrack(long[,] Result, long n, ref List<long> backTrackList)
