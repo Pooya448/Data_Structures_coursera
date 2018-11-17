5f780c66401db1b4782a841d9a0ba981b25a3f44
Pooya448 <pooya_kabiri@yahoo.com>
2018-11-18 00:56:47 +0330

    Q1 Done

---
+++ A8/A8Exec/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A8Exec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "[very(strong]test)";
+            var temp = new A8.CheckBrackets("sd");
+            temp.Solve(str);
+            Console.Read();
+        }
+    }
+}
---
+++ A8/A8Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("A8Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("A8Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d7d717bb-3940-403e-9bf0-d1fe9d361f1e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
