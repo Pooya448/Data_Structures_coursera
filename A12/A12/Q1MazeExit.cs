4665266871dbcb6d0afbce8b6c7fe0b66614eb8d
Pooya448 <pooya_kabiri@yahoo.com>
2019-01-25 05:00:29 +0330

    complete

---
+++ A12/A12/Q1MazeExit.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using TestCommon;
+
+namespace A12
+{
+    public class Q1MazeExit : Processor
+    {
+        public Q1MazeExit(string testDataName) : base(testDataName) { }
+
+        public override string Process(string inStr) =>
+            TestTools.Process(inStr, (Func<long, long[][], long, long, long>)Solve);
+
+        public long Solve(long nodeCount, long[][] edges, long StartNode, long EndNode)
+        {
+            Graph maze = new Graph(nodeCount, edges);
+            return maze.FindPath((int)StartNode, (int)EndNode) ? 1 : 0;
+        }    
+     }
+}
---
+++ A12/A12/Q5StronglyConnected.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using TestCommon;
+
+namespace A12
+{
+    public class Q5StronglyConnected: Processor
+    {
+        public Q5StronglyConnected(string testDataName) : base(testDataName) { }
+
+        public override string Process(string inStr) =>
+            TestTools.Process(inStr, (Func<long, long[][], long>)Solve);
+
+        public long Solve(long nodeCount, long[][] edges)
+        {
+            Graph g = new Graph(nodeCount, edges, true);
+            return g.FindSCCs();
+        }
+    }
+}
